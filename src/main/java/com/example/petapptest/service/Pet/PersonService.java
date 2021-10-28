b7e33e3a2839ff55c7f9f7f693e71141b4cc17a7
TonyJHC <jongvin01@naver.com>
2021-10-28 23:46:41 +0900

    PersonService

---
+++ src/main/java/com/example/petapptest/service/Pet/PersonService.java
@@ -0,0 +1,11 @@
+package com.example.petapptest.service.Pet;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor // 2. final 선언시 꼭 필요
+@Service
+public class PersonService {
+
+}
